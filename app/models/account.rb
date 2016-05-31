b52b2cb3aaa57c2d1cfa61f29234332bc340e7b0
Tommy Hui <hui.1tommy@gmail.com>
2016-05-31 16:23:10 -0400

    fixed schema

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,13 @@
 class Account < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :students
+  has_many :contacts
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :zipcode, presence: true, numericality: true, length: { is: 5 }
+  validates :state, presence: true
 end
--- db/migrate/20160531191111_create_students.rb
+++ db/migrate/20160531191111_create_students.rb
@@ -3,7 +3,9 @@ class CreateStudents < ActiveRecord::Migration[5.0]
     create_table :students do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
+      t.string :school
       t.date :dob, null: false
+      t.belongs_to :account
     end
   end
 end
