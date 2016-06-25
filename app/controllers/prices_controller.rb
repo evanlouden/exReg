205a4900379931d6502b1b5620a26550be8e7801
Anthony DeGennaro <anthonydegennaro@AnthonysMacBook.fios-router.home>
2016-06-24 21:13:19 -0400

    pricing tier tests

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -10,12 +10,34 @@ class PricesController < PermissionsController
   def create
     @price = Price.new(price_params)
     if @price.save
-      flash[:notice] = "Price Added"
+      flash[:notice] = "Pricing Tier Added"
       redirect_to prices_path
     else
       flash[:alert] = @price.errors.full_messages.join(", ")
       render :index
     end
+  end
+
+  def edit
+    @price = Price.find(params[:id])
+  end
+
+  def update
+    @price = Price.find(params[:id])
+    if @price.update(price_params)
+      flash[:notice] = "Pricing Tier Updated"
+      redirect_to prices_path
+    else
+      flash[:alert] = @price.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  def destroy
+    @price = Price.find(params[:id])
+    @price.destroy
+    flash[:notice] = "Pricing Tier Removed"
+    redirect_to prices_path
   end
 
   private
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,7 +1,7 @@
 class Price < ApplicationRecord
-  validates :tier_name, null: false
-  validates :duration, null: false, numericality: { greater_than: 4 }
-  validates :price, null: false, numericality: { greater_than: 0 }
+  validates :tier_name, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 4 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   def description
     "#{tier_name}: #{duration} min., $#{'%.2f' % price}"
