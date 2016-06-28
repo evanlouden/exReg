871e4b401906cf47e2d381e25c6c7c117237de84
Tommy Hui <hui.1tommy@gmail.com>
2016-06-28 18:18:43 -0400

    changed methods to full name

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def full_name
-    "#{self.first_name} #{self.last_name}"
+    "#{first_name} #{last_name}"
   end
 end
