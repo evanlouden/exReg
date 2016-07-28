5d4b7246b613fb731935dc701018a19f68402982
Tommy Hui <hui.1tommy@gmail.com>
2016-07-28 18:29:14 -0400

    drag drop calendar persist

--- app/controllers/api/v1/calendar_controller.rb
+++ app/controllers/api/v1/calendar_controller.rb
@@ -2,13 +2,23 @@ module Api::V1
   class CalendarController < PermissionsController
     # respond_to :json, :js
 
-    def create
+    def index
       @teacher = Teacher.find(params[:id])
       @lessons = @teacher.lessons
       @students = []
       @lessons.map { |l| @students << l.student.full_name }
       response = { lessons: @lessons, students: @students }
       render json: response
+    end
+
+    def create
+      lesson_params = params[:lessons]
+      lesson_params.each do |a|
+        lesson = Lesson.find(a[:id])
+        lesson.day = a[:day]
+        lesson.start_time = a[:start_time]
+        lesson.save
+      end
     end
   end
 end
